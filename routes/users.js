d8e7908c7f41bfe1809e57d2f4013bb113cbe7b9
baozaoxiaoshubao <2596607049@qq.com>
2021-01-23 18:06:44 +0800

    数据库连接成功

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 var express = require('express');
+const { Schema } = require('mongoose');
 var router = express.Router();
+
+
+
+//将用户模板导入
+let User = require('../models/user')
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -14,5 +21,30 @@ router.post('/addUser', (req, res, next) => {
   //用户填写的表单信息可以通过req.body.userName获取
   //res.send('点击注册了')
 
+
+
+  //向用户数据库添加用户信息
+  let userInfo = {
+    userName: req.body.userName,
+    password: req.body.password,
+    passwordC: req.body.passwordC,
+  }
+  //页面表单数据，放入模型
+  let userI = new User(userInfo)
+
+
+  //保存
+  userI.save((err, result)=> {
+    if (!err) {
+      res.send(result)
+    }
+  })
+
+
 })
+
+
+
+
+
 module.exports = router;
